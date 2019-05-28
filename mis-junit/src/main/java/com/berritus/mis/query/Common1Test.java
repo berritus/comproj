ba63e39553402ab0dc5d75816fe0c8ca442671a5
berritus <95559215@qq.com>
2019-05-29 00:59:03 +0800

    add test file

---
+++ mis-junit/src/main/java/com/berritus/mis/query/Common1Test.java
@@ -0,0 +1,60 @@
+package com.berritus.mis.query;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Description:
+ * @Copyright: mis520
+ * @Author: Qin Guihe
+ * @Date: Create in 2019/5/29
+ */
+public class Common1Test {
+
+    class ThreadTest implements Runnable {
+
+        private CountDownLatch countDownLatch;
+
+        public ThreadTest(CountDownLatch countDownLatch) {
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            stringTest();
+        }
+    }
+
+    private void stringTest(){
+        String str = new String("123").intern();
+         synchronized(str) {
+             try {
+                 Thread.sleep(1000);
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+             System.out.println(Thread.currentThread().getName() + " running");
+         }
+    }
+
+    @Test
+    public void test1() throws Exception{
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        for (int i = 0; i < 100; i++) {
+            ThreadTest threadTest = new ThreadTest(countDownLatch);
+
+            new Thread(threadTest).start();
+        }
+
+        countDownLatch.countDown();
+
+        Thread.sleep(100000);
+    }
+}
