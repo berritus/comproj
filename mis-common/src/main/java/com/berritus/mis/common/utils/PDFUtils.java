c0364044afd898f8d2605a48ce7a8e10e0946fe2
覃贵合 <qingh@minstone.com.cn>
2020-03-12 17:45:45 +0800

    20200312

--- mis-common/src/main/java/com/berritus/mis/common/utils/PDFUtils.java
+++ mis-common/src/main/java/com/berritus/mis/common/utils/PDFUtils.java
@@ -7,6 +7,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -14,6 +15,7 @@ import java.util.concurrent.Executors;
 
 import javax.imageio.ImageIO;
 
+import com.lowagie.text.Document;
 import com.minstone.oa.common.thread.OaExecutors;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
@@ -21,12 +23,14 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Element;
-import com.itextpdf.text.Rectangle;
+import com.lowagie.text.Rectangle;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfGState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.Image;
 
 /**
  * @Copyright:
@@ -38,23 +42,211 @@ public class PDFUtils {
 	private static final Logger logger = LoggerFactory.getLogger(PDFUtils.class);
 	public static final float DEFAULT_DPI = 105;
 	private static final int THREAD_PAGE_COUNT = 40;
+
+	public static void main(String[] args) throws Exception {
+		//for (int i = 0; i <= 10; i++) {
+		// 351 32
+		//pdf2png3("E:\\pdf", "32", "png");
+		//pdf2png3old("E:\\pdf","ceshi","png");
+
+		//pdfToImage("E:\\pdf\\32.pdf");
+		//}
+
+		imgToPdf("E:\\pdf\\", "E:\\pdf\\hebing.pdf");
+	}
+
+	public static void imgToPdf(String imageFolderPath, String pdfPath) {
+		long t1 = System.currentTimeMillis();
+		long t2 = System.currentTimeMillis();
+
+		Document doc = null;
+		try {
+			// 图片地址
+			String imagePath = null;
+			// 输入流
+			FileOutputStream fos = new FileOutputStream(pdfPath);
+			// 创建文档
+			doc = new Document(null, 0, 0, 0, 0);
+			// 写入PDF文档
+			PdfWriter.getInstance(doc, fos);
+			// 读取图片流
+			BufferedImage img = null;
+			// 实例化图片
+			Image image = null;
+			// 获取图片文件夹对象
+			File file = new File(imageFolderPath);
+			File[] files = file.listFiles();
+			// 循环获取图片文件夹内的图片
+			for (File file1 : files) {
+				if (file1.getName().endsWith(".png") || file1.getName().endsWith(".jpg")
+						|| file1.getName().endsWith(".gif")
+						|| file1.getName().endsWith(".jpeg")
+						|| file1.getName().endsWith(".tif")) {
+					imagePath = imageFolderPath + file1.getName();
+					logger.info("合并：" + file1.getName());
+					// 读取图片流
+					img = ImageIO.read(new File(imagePath));
+					doc.setPageSize(new Rectangle(img.getWidth(), img.getHeight()));
+					// 实例化图片
+					image = Image.getInstance(imagePath);
+					// 添加图片到文档
+					doc.open();
+					doc.add(image);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("合并异常：{}", e);
+		} finally {
+			if (doc != null && doc.isOpen()) {
+				// 关闭文档
+				doc.close();
+			}
+			t2 = System.currentTimeMillis();
+			logger.info("合并总耗时 times= {} ms", (t2 - t1));
+		}
+	}
+
+	/**
+	 * @Description:
+	 * @Author: Qin Guihe
+	 * @Create: 2020/3/11
+	 * @return:
+	 */
+	private static void pdfToImage(String pdfPath) throws Exception{
+		long t1 = System.currentTimeMillis();
+		long t2 = System.currentTimeMillis();
+		long t3 = System.currentTimeMillis();
+		long t4 = System.currentTimeMillis();
+		long t5 = System.currentTimeMillis();
+
+		CountDownLatch latch = null;
+		PDDocument pdDocument = null;
+		try {
+			if (pdfPath == null || "".equals(pdfPath) || !pdfPath.endsWith(".pdf")) {
+				return;
+			}
+
+			// 保存一张图片中的RGB数据
+			int[] singleImgRGB;
+			// 保存每张图片的像素值
+			BufferedImage imageResult = null;
+			// 利用PdfBox生成图像
+			pdDocument = PDDocument.load(new File(pdfPath));
+			PDFRenderer renderer = new PDFRenderer(pdDocument);
+			int total = pdDocument.getNumberOfPages();
+
+			if (total > 30) {
+				int processNum = Runtime.getRuntime().availableProcessors();
+				ExecutorService executorService = OaExecutors.newFixedThreadPool(processNum - 1);
+				latch = new CountDownLatch(total);
+				for (int i = 0; i < total; i++) {
+					ThreadOriginPdf threadOriginPdf = new ThreadOriginPdf(renderer, i, latch, pdfPath);
+					executorService.submit(threadOriginPdf);
+				}
+				latch.await();
+				executorService.shutdown();
+			} else {
+				// 循环每个页码
+				for (int i = 0; i < total; i++) {
+					pdfToOriginImg(pdfPath, renderer, i);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("转换异常{}", e);
+		} finally {
+			if (pdDocument != null) {
+				pdDocument.close();
+			}
+			t2 = System.currentTimeMillis();
+			logger.info("转换总耗时 times={}", t2 - t1);
+		}
+	}
+
+	/**
+	 * @Description:
+	 * @Author: Qin Guihe
+	 * @Create: 2020/3/11
+	 * @return:
+	 */
+	private static void pdfToOriginImg(String pdfPath, PDFRenderer renderer, Integer pageIndex) throws Exception{
+		long t1 = System.currentTimeMillis();
+		long t2 = System.currentTimeMillis();
+		long t3 = System.currentTimeMillis();
+
+		BufferedImage image = renderer.renderImageWithDPI(pageIndex, DEFAULT_DPI, ImageType.RGB);
+		t2 = System.currentTimeMillis();
+
+		int imageHeight = image.getHeight();
+		int imageWidth = image.getWidth();
+		BufferedImage imageResult = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
+		int[] singleImgRGB = image.getRGB(0, 0, imageWidth, imageHeight, null, 0, imageWidth);
+		// 写入流中
+		imageResult.setRGB(0, 0, imageWidth, imageHeight, singleImgRGB, 0, imageWidth);
+		File outFile = new File(pdfPath.replace(".pdf", "_" + pageIndex + ".jpg"));
+		// 写图片
+		ImageIO.write(imageResult, "jpg", outFile);
+		t3 = System.currentTimeMillis();
+
+		logger.info("pdfToOriginImg ============ t2-t1={}", t2 - t1);
+		logger.info("pdfToOriginImg ============ t3-t2={}", t3 - t2);
+		logger.info("pdfToOriginImg ============ t3-t1={}", t3 - t1);
+
+	}
+
+	/**
+	 * @Description:
+	 * @Author: Qin Guihe
+	 * @Create: 2020/3/11
+	 * @return:
+	 */
+	static class ThreadOriginPdf implements Runnable {
+		private CountDownLatch latch;
+		private PDFRenderer renderer;
+		private int pageIndex;
+		private String pdfPath;
+
+		public ThreadOriginPdf(PDFRenderer renderer, int pageIndex,
+						 CountDownLatch latch, String pdfPath) {
+			this.renderer = renderer;
+			this.pageIndex = pageIndex;
+			this.latch = latch;
+			this.pdfPath = pdfPath;
+		}
+
+		@Override
+		public void run() {
+			long t1 = System.currentTimeMillis();
+			long t2 = System.currentTimeMillis();
+			try {
+				pdfToOriginImg(pdfPath, renderer, pageIndex);
+			} catch (Exception e) {
+				logger.error("ThreadOriginPdf 异常{}, pageIndex={}", e, pageIndex);
+			} finally {
+				latch.countDown();
+				t2 = System.currentTimeMillis();
+				logger.info("ThreadOriginPdf thread={}, use times={}ms", Thread.currentThread().getName(), t2 - t1);
+			}
+		}
+	}
+
 	/**
 	 * 转换全部的pdf
+	 *
 	 * @param fileAddress 文件地址
-	 * @param filename PDF文件名
-	 * @param type 图片类型
+	 * @param filename    PDF文件名
+	 * @param type        图片类型
 	 */
-	public static void pdf2png(String fileAddress,String filename,String type) {
+	public static void pdf2png(String fileAddress, String filename, String type) {
 		// 将pdf装图片 并且自定义图片得格式大小
-		File file = new File(fileAddress+"\\"+filename+".pdf");
+		File file = new File(fileAddress + "\\" + filename + ".pdf");
 		try {
 			PDDocument doc = PDDocument.load(file);
 			PDFRenderer renderer = new PDFRenderer(doc);
 			int pageCount = doc.getNumberOfPages();
 			for (int i = 0; i < pageCount; i++) {
 				BufferedImage image = renderer.renderImageWithDPI(i, 300); // Windows native DPI
-				BufferedImage srcImage = resizeBufferedImage(image,160, 220,false);//产生缩略图
-				ImageIO.write(srcImage, type, new File(fileAddress+"\\"+filename+"_"+(i+1)+"."+type));
+				BufferedImage srcImage = resizeBufferedImage(image, 160, 220, false);//产生缩略图
+				ImageIO.write(srcImage, type, new File(fileAddress + "\\" + filename + "_" + (i + 1) + "." + type));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -63,24 +255,25 @@ public class PDFUtils {
 
 
 	/**
-	 *自由确定起始页和终止页
-	 * @param fileAddress 文件地址
-	 * @param filename pdf文件名
+	 * 自由确定起始页和终止页
+	 *
+	 * @param fileAddress  文件地址
+	 * @param filename     pdf文件名
 	 * @param indexOfStart 开始页  开始转换的页码，从0开始
-	 * @param indexOfEnd 结束页  停止转换的页码，-1为全部
-	 * @param type 图片类型
+	 * @param indexOfEnd   结束页  停止转换的页码，-1为全部
+	 * @param type         图片类型
 	 */
-	public static void pdf2png(String fileAddress,String filename,int indexOfStart,int indexOfEnd,String type) {
+	public static void pdf2png(String fileAddress, String filename, int indexOfStart, int indexOfEnd, String type) {
 		// 将pdf装图片 并且自定义图片得格式大小
-		File file = new File(fileAddress+"\\"+filename+".pdf");
+		File file = new File(fileAddress + "\\" + filename + ".pdf");
 		try {
 			PDDocument doc = PDDocument.load(file);
 			PDFRenderer renderer = new PDFRenderer(doc);
 			int pageCount = doc.getNumberOfPages();
 			for (int i = indexOfStart; i < indexOfEnd; i++) {
 				BufferedImage image = renderer.renderImageWithDPI(i, 160); // Windows native DPI
-				BufferedImage srcImage = resizeBufferedImage(image, 0, 0,true);//产生缩略图
-				ImageIO.write(srcImage, type, new File(fileAddress+"\\"+filename+"_"+(i+1)+"."+type));
+				BufferedImage srcImage = resizeBufferedImage(image, 0, 0, true);//产生缩略图
+				ImageIO.write(srcImage, type, new File(fileAddress + "\\" + filename + "_" + (i + 1) + "." + type));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -91,10 +284,11 @@ public class PDFUtils {
 
 	/**
 	 * 调整bufferedimage大小
-	 * @param source BufferedImage 原始image
+	 *
+	 * @param source  BufferedImage 原始image
 	 * @param targetW int  目标宽
 	 * @param targetH int  目标高
-	 * @param flag boolean 是否同比例调整
+	 * @param flag    boolean 是否同比例调整
 	 * @return BufferedImage  返回新image
 	 */
 	private static BufferedImage resizeBufferedImage(BufferedImage source, int targetW, int targetH, boolean flag) {
@@ -105,7 +299,7 @@ public class PDFUtils {
 		if (flag && sx > sy) {
 			sx = sy;
 			targetW = (int) (sx * source.getWidth());
-		} else if(flag && sx <= sy){
+		} else if (flag && sx <= sy) {
 			sy = sx;
 			targetH = (int) (sy * source.getHeight());
 		}
@@ -126,13 +320,14 @@ public class PDFUtils {
 
 	/**
 	 * 绘制水印的方法
+	 *
 	 * @param content
 	 * @param gs
 	 * @param pageRect
 	 * @param watermark
 	 */
-	public static void  addWatermark(PdfContentByte content, PdfGState gs, Rectangle pageRect,
-									 BaseFont font, String watermark) {
+	public static void addWatermark(PdfContentByte content, PdfGState gs, Rectangle pageRect,
+									BaseFont font, String watermark) {
 		try {
 			float x = pageRect.getWidth() / 2.0F;
 			float y = pageRect.getHeight() / 2.0F;
@@ -145,36 +340,26 @@ public class PDFUtils {
 			content.endText();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			content = null;
 			font = null;
 			pageRect = null;
 		}
 	}
 
-	public static void main(String[] args)throws Exception {
-		//for (int i = 0; i <= 10; i++) {
-			// 351 32
-			pdf2png3("E:\\pdf","32","png");
-			//pdf2png3old("E:\\pdf","ceshi","png");
-
-			// pdfToImage2("E:\\pdf\\32.pdf");
-		//}
-	}
-
 	/**
 	 * 转换全部的pdf
+	 *
 	 * @param fileAddress 文件地址
-	 * @param filename PDF文件名
-	 * @param type 图片类型
+	 * @param filename    PDF文件名
+	 * @param type        图片类型
 	 */
-	public static void pdf2png3(final String fileAddress,final String filename,
+	public static void pdf2png3(final String fileAddress, final String filename,
 								final String type) throws Exception {
-
 		long t1 = System.currentTimeMillis();
 		long t2 = System.currentTimeMillis();
 		// 将pdf装图片 并且自定义图片得格式大小
-		File file = new File(fileAddress+"\\"+filename+".pdf");
+		File file = new File(fileAddress + "\\" + filename + ".pdf");
 		CountDownLatch latch = null;
 		try {
 			PDDocument doc = PDDocument.load(file);
@@ -182,25 +367,23 @@ public class PDFUtils {
 			Integer pageCount = doc.getNumberOfPages();
 
 			//pageCount = pageCount - 1;
-			if (pageCount >= 30) {
+			if (pageCount >= 40) {
 				latch = new CountDownLatch(pageCount);
 				int processNum = Runtime.getRuntime().availableProcessors();
 				ExecutorService executorService = OaExecutors.newFixedThreadPool(processNum - 1);
 				//ExecutorService executorService = Executors.newFixedThreadPool(10);
 				for (int i = 0; i < pageCount; i++) {
-					final int pageIndex = i ;
-					ThreadPdf threadPdf = new ThreadPdf(renderer, doc, pageIndex, latch, fileAddress, filename, type);
+					ThreadPdf threadPdf = new ThreadPdf(renderer, doc, i, latch, fileAddress, filename, type);
 					executorService.submit(threadPdf);
 				}
 				executorService.shutdown();
 			} else {
 				for (int i = 0; i < pageCount; i++) {
-					final int pageIndex = i ;
-					pdf2pngHandler(renderer, doc, pageIndex, fileAddress, filename, type);
+					pdf2pngHandler(renderer, doc, i, fileAddress, filename, type);
 				}
+
+				//pdf2pngHandler(renderer, doc, 0, fileAddress, filename, type);
 			}
-
-
 		} catch (IOException e) {
 			logger.error("异常{}", e);
 		}
@@ -229,7 +412,7 @@ public class PDFUtils {
 		private String type;
 
 		public ThreadPdf(PDFRenderer renderer, PDDocument doc, int pageIndex,
-						 CountDownLatch latch,String fileAddress, String filename, String type) {
+						 CountDownLatch latch, String fileAddress, String filename, String type) {
 			this.renderer = renderer;
 			this.doc = doc;
 			this.pageIndex = pageIndex;
@@ -262,7 +445,7 @@ public class PDFUtils {
 	 * @return:
 	 */
 	private static void pdf2pngHandler(PDFRenderer renderer, PDDocument doc, int pageIndex,
-									   String fileAddress,String filename, String type) throws Exception{
+									   String fileAddress, String filename, String type) throws Exception {
 		long t1 = System.currentTimeMillis();
 		long t2 = System.currentTimeMillis();
 		long t3 = System.currentTimeMillis();
@@ -271,138 +454,14 @@ public class PDFUtils {
 		BufferedImage image = renderer.renderImageWithDPI(pageIndex, 144, ImageType.RGB);
 		t2 = System.currentTimeMillis();
 		//产生缩略图
-		BufferedImage srcImage = resizeBufferedImage(image,160, 220,false);
+		BufferedImage srcImage = resizeBufferedImage(image, 160, 220, false);
 		t3 = System.currentTimeMillis();
-		ImageIO.write(srcImage, type, new File(fileAddress+"\\"+filename+"_"+(pageIndex+1)+"."+type));
+		ImageIO.write(srcImage, type, new File(fileAddress + "\\" + filename + "_" + (pageIndex + 1) + "." + type));
 		t4 = System.currentTimeMillis();
 
 		logger.info("thread={} pageIndex={},t2 - t1 use times={}ms", Thread.currentThread().getName(), pageIndex, t2 - t1);
 		logger.info("thread={} pageIndex={},t3 - t2 use times={}ms", Thread.currentThread().getName(), pageIndex, t3 - t2);
 		logger.info("thread={} pageIndex={},t4 - t3 use times={}ms", Thread.currentThread().getName(), pageIndex, t4 - t3);
 		logger.info("thread={} pageIndex={},t4 - t1 use times={}ms", Thread.currentThread().getName(), pageIndex, t4 - t1);
-	}
-
-	public static void pdf2png3old(final String fileAddress,final String filename,final String type) {
-		long t1 = System.currentTimeMillis();
-		long t2 = System.currentTimeMillis();
-		long t3 = System.currentTimeMillis();
-		long t4 = System.currentTimeMillis();
-		long t5 = System.currentTimeMillis();
-		long t6 = System.currentTimeMillis();
-		// 将pdf装图片 并且自定义图片得格式大小
-		File file = new File(fileAddress+"\\"+filename+".pdf");
-		try {
-			PDDocument doc = PDDocument.load(file);
-			final PDFRenderer renderer = new PDFRenderer(doc);
-			Integer pageCount = doc.getNumberOfPages();
-
-			for (int i = 0; i < pageCount; i++) {
-				final int pageIndex = i ;
-				try {
-					// 144 105
-					t3 = System.currentTimeMillis();
-					BufferedImage image = renderer.renderImageWithDPI(pageIndex, 105); // Windows native DPI
-					t4 = System.currentTimeMillis();
-					logger.info("=============t4-t3={}", t4-t3);
-					BufferedImage srcImage = resizeBufferedImage(image,160, 220,false);//产生缩略图
-					t5 = System.currentTimeMillis();
-					logger.info("=============t5-t4={}", t5-t4);
-					ImageIO.write(srcImage, type, new File(fileAddress+"\\"+filename+"_"+(pageIndex+1)+"."+type));
-					t6 = System.currentTimeMillis();
-					logger.info("=============t6-t5={}", t6-t5);
-				}catch (Exception e) {
-					return;
-				}
-			}
-
-//			ExecutorService executorService = Executors.newFixedThreadPool(10);
-//			for (int i = 0; i < pageCount; i++) {
-//				final int pageIndex = i ;
-//				executorService.submit(new Runnable() {
-//					@Override
-//					public void run() {
-//						try {
-//							BufferedImage image = renderer.renderImageWithDPI(pageIndex, 144); // Windows native DPI
-//							BufferedImage srcImage = resizeBufferedImage(image,160, 220,false);//产生缩略图
-//							ImageIO.write(srcImage, type, new File(fileAddress+"\\"+filename+"_"+(pageIndex+1)+"."+type));
-//						}catch (Exception e) {
-//							return;
-//						}
-//					}
-//				});
-//
-//			}
-//			executorService.shutdown();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		t2 = System.currentTimeMillis();
-		System.out.println("============timesss=" + (t2 - t1));
-	}
-
-	private static void pdfToImage2(String pdfPath) {
-		long t1 = System.currentTimeMillis();
-		long t2 = System.currentTimeMillis();
-		long t3 = System.currentTimeMillis();
-		long t4 = System.currentTimeMillis();
-		long t5 = System.currentTimeMillis();
-
-		try {
-			if (pdfPath == null || "".equals(pdfPath) || !pdfPath.endsWith(".pdf")) {
-				return;
-			}
-
-			// 图像合并使用参数
-			int width = 0; // 总宽度
-			int[] singleImgRGB; // 保存一张图片中的RGB数据
-			int shiftHeight = 0;
-			BufferedImage imageResult = null;// 保存每张图片的像素值
-			// 利用PdfBox生成图像
-			PDDocument pdDocument = PDDocument.load(new File(pdfPath));
-			PDFRenderer renderer = new PDFRenderer(pdDocument);
-			// 循环每个页码
-			for (int i = 0, len = pdDocument.getNumberOfPages(); i < len; i++) {
-				//BufferedImage srcImage = resizeBufferedImage(image,160, 220,false);/
-				t2 = System.currentTimeMillis();
-				BufferedImage image = renderer.renderImageWithDPI(i, DEFAULT_DPI, ImageType.RGB);
-				t3 = System.currentTimeMillis();
-				logger.info("============t3-t2={}", t3 - t2);
-				int imageHeight = image.getHeight();
-				int imageWidth = image.getWidth();
-
-				imageResult = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
-				singleImgRGB = image.getRGB(0, 0, imageWidth, imageHeight, null, 0, imageWidth);
-				//imageResult.setRGB(0, 0, imageWidth, imageHeight, singleImgRGB, 0, imageWidth); // 写入流中
-
-				imageResult.setRGB(0, 0, imageWidth, imageHeight, singleImgRGB, 0, imageWidth); // 写入流中
-
-				File outFile = new File(pdfPath.replace(".pdf", "_" + DEFAULT_DPI + "_" + i + ".jpg"));
-				ImageIO.write(imageResult, "jpg", outFile);// 写图片
-				//break;
-				// old
-//				if (i == 0) {// 计算高度和偏移量
-//					width = imageWidth;// 使用第一张图片宽度;
-//					// 保存每页图片的像素值
-//					imageResult = new BufferedImage(width, imageHeight * len, BufferedImage.TYPE_INT_RGB);
-//					//imageResult = new BufferedImage(160, 220, BufferedImage.TYPE_INT_RGB);
-//				} else {
-//					shiftHeight += imageHeight; // 计算偏移高度
-//				}
-//				singleImgRGB = image.getRGB(0, 0, width, imageHeight, null, 0, width);
-//				imageResult.setRGB(0, shiftHeight, width, imageHeight, singleImgRGB, 0, width); // 写入流中
-//				//break;
-			}
-
-			pdDocument.close();
-			// old
-//			File outFile = new File(pdfPath.replace(".pdf", "_" + DEFAULT_DPI + ".jpg"));
-//			ImageIO.write(imageResult, "jpg", outFile);// 写图片
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			t2 = System.currentTimeMillis();
-			logger.info("============timesss={}", t2 - t1);
-		}
 	}
 }
